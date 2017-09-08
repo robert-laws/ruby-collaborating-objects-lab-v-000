f7cf0bfa77ddf670538df745c8037d67181e8628
Robert Laws <roblaws@me.com>
2017-09-08 12:14:12 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,10 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :music_importer
 
   def initialize(path)
     @path = path
+    @music_importer = Dir.glob(path + "*")
   end
+
+
 end
