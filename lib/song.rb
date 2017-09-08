d89da38bce10dbb305489647ecd80c641d6fcb8d
Robert Laws <roblaws@me.com>
2017-09-08 14:19:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def self.new_by_filename(file_name)
     parts = file_name.split(" - ")
     song = Song.new(parts[1])
-    self.artist_name(parts[0])
+    @artist = self.artist_name(parts[0])
     song
   end
 
