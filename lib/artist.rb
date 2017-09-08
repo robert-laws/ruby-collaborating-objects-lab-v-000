b7aec654d113ceb1e2d35f0129284107e00a0d8b
Robert Laws <roblaws@me.com>
2017-09-08 12:09:39 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,41 @@
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    result = ""
+    all.each do |a|
+      if a.name == name
+        result = a
+        break
+      else
+        result = Artist.new(name)
+      end
+    end
+    result
+  end
+
+  def print_songs
+    songs.each do |song|
+      puts song.name
+    end
+  end
+
+  def save
+    @@all << self
+  end
+end
