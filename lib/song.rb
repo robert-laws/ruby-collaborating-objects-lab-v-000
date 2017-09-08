bfea2ae39b6b3e1955b53961dcde5929738858ed
Robert Laws <roblaws@me.com>
2017-09-08 14:22:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
     song
   end
 
-  def artist_name(artist_name)
+  def self.artist_name(artist_name)
     artist = Artist.find_or_create_by_name(artist_name)
     artist.add_song(self)
   end
