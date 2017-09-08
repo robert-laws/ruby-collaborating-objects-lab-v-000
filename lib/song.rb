2081473c72d680c4bb7433050ba353c6712e127e
Robert Laws <roblaws@me.com>
2017-09-08 13:50:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,14 @@ class Song
   def initialize(name)
     @name = name
   end
+
+  def new_by_filename(file_name)
+    parts = file_name.split(" - ")
+    song = Song.new(parts[1])
+    
+  end
+
+  def artist_name(artist_name)
+    @artist = artist_name
+  end
 end
